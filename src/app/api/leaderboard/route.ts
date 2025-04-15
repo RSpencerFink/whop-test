2a0f457f1260a2cbaa43191e2c080b1ff94968d7
R. Spencer Fink <Fink.Spencer@Gmail.com>
2025-04-15 16:20:39 -0400

    Enhance leaderboard API to accept a limit parameter for results, improving query flexibility.

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,6 +1,10 @@
 import { caller } from "~/trpc/server";
 
-export async function GET(_request: Request) {
-  const leaderboard = await caller.profile.getLeaderboard({});
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const limit = searchParams.get("limit") ?? "10";
+  const leaderboard = await caller.profile.getLeaderboard({
+    limit: parseInt(limit),
+  });
   return new Response(JSON.stringify(leaderboard), { status: 200 });
 }
