8f4c96876d7a412a69f8cc3046d6b15988a55609
R. Spencer Fink <Fink.Spencer@Gmail.com>
2025-04-15 18:10:17 -0400

    Add transaction schema and status enum to database

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,6 +8,7 @@ import {
   pgTableCreator,
   timestamp,
   varchar,
+  pgEnum,
 } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
@@ -18,6 +19,12 @@ import { createInsertSchema, createSelectSchema } from "drizzle-zod";
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
 export const createTable = pgTableCreator((name) => `whop-test_${name}`);
+
+export const transactionStatus = pgEnum("transaction_status", [
+  "pending",
+  "completed",
+  "failed",
+]);
 
 const timestamps = {
   createdAt: timestamp({ withTimezone: true })
@@ -71,3 +78,26 @@ export const walletsInsertSchema = createInsertSchema(wallets);
 export const walletsRelations = relations(wallets, ({ one }) => ({
   profile: one(profiles),
 }));
+
+export const transactions = createTable("transactions", {
+  id: integer().primaryKey().generatedByDefaultAsIdentity(),
+  recipientId: integer()
+    .references(() => profiles.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  senderId: integer()
+    .references(() => profiles.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  amount: integer().notNull(),
+  status: transactionStatus("pending").notNull(),
+  ...timestamps,
+});
+
+export type Transactions = typeof transactions.$inferSelect;
+export type NewTransactions = typeof transactions.$inferInsert;
+
+export const transactionsSelectSchema = createSelectSchema(transactions);
+export const transactionsInsertSchema = createInsertSchema(transactions);
